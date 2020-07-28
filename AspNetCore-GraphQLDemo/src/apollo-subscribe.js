0ee097d92087a562e5b14e37d21bab037da05b05
Tore Aurstad <tore.aurstad@hemit.no>
2020-07-28 19:25:57 +0200

    Can now use subscriptions. Tested with multiple with multiple clients in different browser windows. Uses Web sockets

---
+++ AspNetCore-GraphQLDemo/src/apollo-subscribe.js
@@ -0,0 +1,47 @@
+﻿////import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
+//import { ApolloClient, createNetworkInterface } from '@apollo/client';
+
+
+//function ConnectToWebSocket() {
+//// Create regular NetworkInterface by using apollo-client's API:
+//	const networkInterface = createNetworkInterface({
+//		uri: 'http://localhost:2542' // Your GraphQL endpoint
+//	});
+
+////// Create WebSocket client
+////	const wsClient = new SubscriptionClient(`ws://localhost:2452/`,
+////		{
+////			reconnect: true,
+////			connectionParams: {
+////				// Pass any arguments you want for initialization
+	
+////			}
+////		});
+
+////// Extend the network interface with the WebSocket
+////	const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+////		networkInterface,
+////		wsClient
+////	);
+
+////// Finally, create your ApolloClient instance with the modified network interface
+////	const apolloClient = new ApolloClient({
+////		networkInterface: networkInterfaceWithSubscriptions
+////	});
+////	return apolloClient;
+//}
+
+//function Subscribe(client, gql, vars, callbackFun) {
+//	client.subscribe({
+//		query: gql`
+//   subscription {detailsDisplayed {id} }`,
+//		variables: vars
+//	}).subscribe({
+//		next(data) {
+//			// Notify your application with the new arrived data
+//			callbackFun(data);
+//		}
+//	});
+
+//}
+
--- AspNetCore-GraphQLDemo/src/index.js
+++ AspNetCore-GraphQLDemo/src/index.js
@@ -1,7 +1,84 @@
 ﻿import _ from 'lodash';
+//import * as ConnectDemo from './apollo-subscribe'; 
+import { ApolloClient, createNetworkInterface, gql } from 'apollo-client';
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
+
+
+//import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
+
+
+function ConnectToWebSocket() {
+    // Create regular NetworkInterface by using apollo-client's API:
+   
+    const GRAPHQL_ENDPOINT = `http://localhost:2542/graphql`;
+    const GRAPHQL_SUBSCRIPTION_ENDPOINT = `ws://localhost:2542/graphql`;
+
+    //// Create WebSocket client
+    const wsClient = new SubscriptionClient(`${GRAPHQL_SUBSCRIPTION_ENDPOINT}`,
+        {
+            reconnect: true,
+            connectionParams: {
+                // Pass any arguments you want for initialization
+            }
+        });
+
+    const networkInterface = createNetworkInterface({
+	    uri: GRAPHQL_ENDPOINT,
+	    opts: {
+		    credentials: `same- origin`
+	    },
+    });
+
+    //////// Extend the network interface with the WebSocket
+    //const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+    //    networkInterface,
+    //    wsClient
+    //);
+
+    const client = new ApolloClient({
+	    networkInterface: wsClient
+	    // …
+    });
+
+    return client;
+
+}
+
+function Subscribe(client, gql, vars, callbackFun) {
+
+     
+    client.subscribe({
+        query: Apollo.gql`
+   subscription {detailsDisplayed {id} }`,
+        variables: vars
+    }).subscribe({
+        next(data) {
+            // Notify your application with the new arrived data
+            callbackFun(data);
+        }
+    });
+
+}
+
 function createChild() {
-	var element = document.createElement('div');
-	element.innerHTML = _.join(['Testing', 'Webpack'], ' ');
-	return element;
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    return element;
 }
 document.body.appendChild(createChild());
+
+const DETAILS_SHOWED = Apollo.gql(`subscription {detailsDisplayed {id} }`);
+
+
+function callBackConnectDemo(data) {
+    if (!!data && !!data.detailsDisplayed) {
+	    toastr.info(`A user selected to display the mountain with ID: ${data.detailsDisplayed.id} `);
+    }
+}
+
+
+var client = ConnectToWebSocket();
+
+Subscribe(client, DETAILS_SHOWED, {}, callBackConnectDemo);
+
+
